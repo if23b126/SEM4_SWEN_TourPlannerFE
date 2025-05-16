398e64222de4fb314b2e26b138bb6fb3cf1fdcbc
Christian Eder <c.eder@aon.at>
2025-05-16 11:23:05 +0200

    started on angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {StartComponent} from './start/start.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'start', component: StartComponent }
+];
